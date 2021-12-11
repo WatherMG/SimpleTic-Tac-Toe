e8c3e3756783db1d670eeeff9d199e4fbd437ca1
WatherMG <dr.wather@gmail.com>
2021-12-11 21:54:20 +0300

    Stage 2/5: The user is the gamemaster

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,2 +1,7 @@
-print("X X X")
-print("O O O")
+user_input = input("Enter cells: ")
+
+print("---------")
+print(f"| {user_input[0]} {user_input[1]} {user_input[2]} |")
+print(f"| {user_input[3]} {user_input[4]} {user_input[5]} |")
+print(f"| {user_input[6]} {user_input[7]} {user_input[8]} |")
+print("---------")
